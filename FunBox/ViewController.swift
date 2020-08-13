111e61ee183c384196f5b5c407b1ea6db470eeb0
Denis Prozukin <dprozukin@gmail.com>
2020-08-13 19:51:06 +0300

    Initial Commit

---
+++ FunBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunBox
+//
+//  Created by Denis Prozukin on 13.08.2020.
+//  Copyright © 2020 Denis Prozukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
